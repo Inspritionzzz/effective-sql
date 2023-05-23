51c09ad2dae1e220d3b6e048ff7cb8e6d8cc9877
jason <591831416@qq.com>
2023-05-23 11:18:16 +0800

    update-20230523

---
+++ create_table.sql
@@ -0,0 +1,21 @@
+
+
+-- CREATE TABLE "public"."manual_import_cust_name_duplicate_20230515" (
+--   "cst_code" varchar(255) COLLATE "pg_catalog"."default",
+--   "cst_name" varchar(255) COLLATE "pg_catalog"."default",
+--   "registration_state" varchar(255) COLLATE "pg_catalog"."default",
+--   "cst_code_zixun" varchar(255) COLLATE "pg_catalog"."default"
+-- );
+
+select * from manual_import_cust_name_duplicate_20230515;
+
+
+CREATE TABLE "public"."manual_import_cust_name_duplicate_20230515" (
+  "cst_code" varchar(255) COLLATE "pg_catalog"."default",
+  "cst_name" varchar(255) COLLATE "pg_catalog"."default",
+  "registration_state" varchar(255) COLLATE "pg_catalog"."default",
+  "cst_code_zixun" varchar(255) COLLATE "pg_catalog"."default"
+);
+
+
+select * from data;
--- scratch.sql
+++ scratch.sql
@@ -1,6 +1,15 @@
+CREATE TABLE "dmddata"."dq_chk_ccm_zxjtcrm_jg_cst_basic_info_result" (
+  "cst_code" varchar(255) COLLATE "pg_catalog"."default" DEFAULT NULL NOT NULL,
+  "cst_name" varchar(255) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "cert_no" varchar(255) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "cert_type" varchar(255) COLLATE "pg_catalog"."default" DEFAULT NULL,  -- 字典值参考：证件类型代码表：CERT_TYPE_CD
+  "registration_state" varchar(255) COLLATE "pg_catalog"."default" DEFAULT NULL,
+  "deal_way" varchar(255) COLLATE "pg_catalog"."default" DEFAULT NULL,   -- 处理方式
+  "prob_rmk" varchar(255) COLLATE "pg_catalog"."default" DEFAULT NULL,   -- 问题备注
+  "recdate" date DEFAULT NULL
+);
+comment on table dmddata.dq_chk_ccm_zxjtcrm_jg_cst_basic_info_result is 'CCRM外部实体表脏数据处理结果表';
 
 
 
-
-select *
-from data;
+ALTER TABLE dmddata.dq_chk_ccm_zxjtcrm_jg_cst_basic_info_result  ALTER COLUMN recdate  TYPE integer USING (recdate::integer);
