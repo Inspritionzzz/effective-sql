5e3e4dc2f3450038dcdd7577a87241adaab48573
jason <591831416@qq.com>
2024-01-17 13:25:15 +0800

    update-20240117

---
+++ postgresql/sql-script-note.sql
@@ -0,0 +1,49 @@
+-- 日期函数（参考：https://blog.csdn.net/weixin_40594160/article/details/100139852）
+select now();
+
+select current_timestamp;
+
+select current_date;
+
+select current_time;
+
+select now() + interval '2 years';  -- 2015-04-12 15:49:03.168851+08 (1 row)
+
+select now() + interval '2 year'; -- 2015-04-12 15:49:12.378727+08 (1 row)
+
+select now() + interval '2 y'; -- 2015-04-12 15:49:25.46986+08 (1 row)
+
+select now() + interval '2 Y'; -- 2015-04-12 15:49:28.410853+08 (1 row)
+
+select now() + interval '2Y'; -- 2015-04-12 15:49:31.122831+08 (1 row)
+
+select now() + interval '1 month';
+
+select now() - interval '3 week';
+
+-- 说明：
+-- interval 可以不写，其值可以是：
+-- Abbreviation	Meaning
+-- Y	Years
+-- M	Months (in the date part)
+-- W	Weeks
+-- D	Days
+-- H	Hours
+-- M	Minutes (in the time part)
+-- S	Seconds
+
+select age(timestamp '2007-09-15');
+
+select extract(year from now());
+
+select extract(doy from now());
+
+select extract(epoch from now());
+
+select now() + '10 min';
+
+select timestamp with time zone 'epoch' + 1369755555 * interval '1 second';
+
+select cast(to_char(current_date, 'yyyymmdd') as integer); -- 当日
+
+select cast(to_char(current_date - interval '1 day', 'yyyymmdd') as integer); -- 昨日
